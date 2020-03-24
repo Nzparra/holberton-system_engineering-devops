f4c0745dea01305ed5e55881f6a08735dd763b6c
Nicolas <nzparra@gmail.com>
2020-03-23 19:16:58 -0500

    [Update]Style

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # create a manifest that kills a process named killmenow.
 
 exec { 'process_kill':
-    path    => ['/user/bin', '/bin', '/usr/sbin', '/sbin'],
-    command => 'pkill killmenow'}
+    path     => ['/user/bin', '/bin', '/usr/sbin', '/sbin'],
+    command  => 'pkill killmenow',
+    provider => 'shell' }
